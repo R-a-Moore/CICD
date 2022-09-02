3e435a8ea4bf307329c6faa4b63436c83d119449
Christian <Moore.Christian76@gmail.com>
2022-09-02 17:52:36 +0100

    new rev proxy

--- provisions/provision.sh
+++ provisions/provision.sh
@@ -1,6 +1,6 @@
 sudo apt-get update -y
   
- sudo apt-get upgrade -y
+sudo apt-get upgrade -y
   
 # installs nginx
 sudo apt-get install nginx -y
@@ -12,12 +12,6 @@ curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
 sudo apt-get install -y nodejs npm
 
-#back into base directory
-cd
-
-# moves into app
-cd app
-
 # replaces default with reverse proxy
 sudo cp -f rev_proxy /etc/nginx/sites-available/default
 
@@ -26,5 +20,11 @@ sudo systemctl restart nginx
 sudo apt-get update -y
 
 sudo apt-get upgrade -y
+
+# moves into app
+#back into base directory
+cd
+cd app
+
  #installs npm
 sudo npm install
